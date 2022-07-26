c6daedb9e5d7753710434a57e6825273a1c346f9
zequihg50 <ezequiel.cimadevilla@unican.es>
2022-07-26 17:04:12 +0200

    merged tds5 with udg

--- roles/udg/files/build.sh
+++ roles/udg/files/build.sh
@@ -8,16 +8,17 @@ BUILDDIR=build
 
 rm -rf ${BUILDDIR}
 mkdir -p ${BUILDDIR}
-cp udg-tap.war javamelody-core-1.73.1.jar jrobin-1.5.9.jar ${BUILDDIR}
+cp udg-tap.war ${BUILDDIR}
 
 cd ${BUILDDIR}
 rm -rf udg
 
 # download
-#wget -qc -O tomcat.tar.gz https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.62/bin/apache-tomcat-9.0.62.tar.gz
-wget -qc -O tomcat.tar.gz https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.78/bin/apache-tomcat-8.5.78.tar.gz
+wget -qc -O javamelody-core.jar https://github.com/javamelody/javamelody/releases/download/javamelody-core-1.91.0/javamelody-core-1.91.0.jar
+wget -qc -O jrobin.jar https://master.dl.sourceforge.net/project/jrobin/OldFiles/jrobin-1.5.9.jar
+wget -qc -O tomcat.tar.gz https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.81/bin/apache-tomcat-8.5.81.tar.gz
 wget -qc -O derbydb.tar.gz https://ftp.cixug.es/apache/db/derby/db-derby-10.14.2.0/db-derby-10.14.2.0-lib.tar.gz
-wget -qc -O tds.war https://downloads.unidata.ucar.edu/tds/5.4/thredds-5.4-SNAPSHOT.war
+wget -qc -O tds.war https://downloads.unidata.ucar.edu/tds/5.4/thredds-5.4.war
 
 mkdir udg
 tar -C udg --strip-components=1 -xf tomcat.tar.gz
@@ -28,14 +29,14 @@ rm -f udg/{BUILDING.txt,CONTRIBUTING.md,LICENSE,README.md,NOTICE,RELEASE-NOTES,R
 # tds
 unzip -q -d udg/webapps/thredds tds.war
 tar -xf derbydb.tar.gz -C udg/lib --strip-components=2
-cp jrobin-1.5.9.jar javamelody-core-1.73.1.jar udg/webapps/thredds/WEB-INF/lib/
+cp jrobin.jar javamelody-core.jar udg/webapps/thredds/WEB-INF/lib/
 
 # tap
 unzip -q -d udg/webapps/udg-tap udg-tap.war
 
 # package
 cd udg
-zip -qr udgtomcat.zip *
+zip -qr udg.zip *
 
 cd ${CURRENTDIR}
-mv ${BUILDDIR}/udg/udgtomcat.zip .
+mv ${BUILDDIR}/udg/udg.zip .
