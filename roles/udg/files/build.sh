1ef375bfc00b6bf10ced83941b163c6ad018dd17
zequihg50 <ezequiel.cimadevilla@unican.es>
2022-07-29 10:55:13 +0200

    changed tds version due to errors

--- roles/udg/files/build.sh
+++ roles/udg/files/build.sh
@@ -18,7 +18,8 @@ wget -qc -O javamelody-core.jar https://github.com/javamelody/javamelody/release
 wget -qc -O jrobin.jar https://master.dl.sourceforge.net/project/jrobin/OldFiles/jrobin-1.5.9.jar
 wget -qc -O tomcat.tar.gz https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.81/bin/apache-tomcat-8.5.81.tar.gz
 wget -qc -O derbydb.tar.gz https://ftp.cixug.es/apache/db/derby/db-derby-10.14.2.0/db-derby-10.14.2.0-lib.tar.gz
-wget -qc -O tds.war https://downloads.unidata.ucar.edu/tds/5.4/thredds-5.4.war
+#wget -qc -O tds.war https://downloads.unidata.ucar.edu/tds/5.4/thredds-5.4.war
+wget -qc -O tds.war https://artifacts.unidata.ucar.edu/repository/unidata-releases/edu/ucar/tds/5.0.0-beta7/tds-5.0.0-beta7.war
 
 mkdir udg
 tar -C udg --strip-components=1 -xf tomcat.tar.gz
